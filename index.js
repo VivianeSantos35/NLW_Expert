3cb21e04710d1a3b691f10daad6424464d60aa2a
VivianeSantos35 <vivyane-santos@hotmail.com>
2024-02-18 19:24:05 -0300

    Alterações no código

--- index.js
+++ index.js
@@ -100,20 +100,20 @@ const perguntas = [
   mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
   // loop ou laço de repetição
-  for(const item of perguntas) {
+  for (const item of perguntas) {
     const quizItem = template.content.cloneNode(true)
     quizItem.querySelector('h3').textContent = item.pergunta
 
-    for(let resposta of item.respostas) {
+    for (let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
-        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
-        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))  
+        dt.querySelector('input').value = item.respostas.indexOf(resposta) 
         dt.querySelector('input').onchance = (event) => {
             const estaCorreta = event.target.value == item.correta
 
             corretas.delete(item)
-            if(estaCorreta) {
+            if (estaCorreta) {
                 corretas.add(item)
             }
 
@@ -126,6 +126,6 @@ const perguntas = [
     quizItem.querySelector('dl dt').remove()
 
 
-    // Coloca a pergunta na tela
+    // coloca a pergunta na tela
     quiz.appendChild(quizItem)
   }
